e6fb40430be4e44d0d473900347385103589dcc2
Mateus Dias <mateuussilvapb@gmail.com>
2024-04-15 15:43:28 -0300

    Feature: Configuração inicial do projeto

---
+++ src/main/java/io/github/mateuussilvapb/app_corretores/AppCorretoresApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateuussilvapb.app_corretores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCorretoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCorretoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateuussilvapb/app_corretores/AppCorretoresApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateuussilvapb.app_corretores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCorretoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
