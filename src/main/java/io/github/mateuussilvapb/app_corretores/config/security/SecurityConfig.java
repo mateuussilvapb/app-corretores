e2e21c5759dc2431b8d1b67dfffb759dea5d2c0a
Mateus Dias <mateuussilvapb@gmail.com>
2024-05-06 09:25:12 -0300

    Feature: Ajustes de configurações de segurança

--- src/main/java/io/github/mateuussilvapb/app_corretores/config/security/SecurityConfig.java
+++ src/main/java/io/github/mateuussilvapb/app_corretores/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.github.mateuussilvapb.app_corretores.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -7,6 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -14,11 +20,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String corsAllowedOrigins;
+
     private static final String[] ALLOW_ACCESS = {"/h2-console/**", "/token"};
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors().configurationSource(corsConfigurationSource()).and()
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())))
                 .headers(headers -> headers.frameOptions().sameOrigin()) // Allow same origin to frame our site
                 .authorizeHttpRequests(authorize ->
@@ -29,5 +39,17 @@ public class SecurityConfig {
                                 .authenticated()
                 );
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(corsAllowedOrigins.split(",")));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
